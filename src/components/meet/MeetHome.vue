163ff9f025314dc180ea4434835081fc20d95dff
Thiago Leite <leite.tjs@gmail.com>
2023-11-27 21:40:49 -0300

    feat: basic structure do mobile and desk

---
+++ src/components/meet/MeetHome.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
+
+<template>
+  <div class="container-principal">
+    <div class="container-meet"></div>
+  </div>
+</template>
+
+<style src="../../assets/styles/principal.scss" lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
 import FooterComponent from '@/components/general/FooterComponent.vue';
 import HeaderComponent from '@/components/general/HeaderComponent.vue';
+import MeetHome from '@/components/meet/MeetHome.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  components: { HeaderComponent, FooterComponent }
+  components: { HeaderComponent, FooterComponent, MeetHome }
 });
 </script>
 
 <template>
   <HeaderComponent />
+  <MeetHome />
   <FooterComponent />
 </template>
 
